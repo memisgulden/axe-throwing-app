550056aba07dbe70fab065f6a8cc095e0c0712e4
memisgulden <memis@gmail.com>
2019-01-09 15:15:57 -0500

    auth

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
+var session = require("express-session");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
+
+
 //middleware for parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,12 +19,37 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/axe-throwing-app");
 const db = require("./models");
 
+//set up passport here
+app.use(session({ secret: "keyboard", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 //login post route
-app.post("/login", function(req,res){
-  console.log("login route hit");
-  console.log(req.body);
-  res.json({loggedIn: true});
-})
+
+app.post("/login", passport.authenticate("local"), function(req, res) {
+  // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
+  // So we're sending the user back the route to the members page because the redirect will happen on the front end
+  // They won't get this or even be able to access this page if they aren't authed
+  // res.json("/members");
+  res.json({success: true, message: "woo it worked"});
+});
+
+ // Route for getting some data about our user to be used client side
+//  app.get("/api/user_data", function(req, res) {
+//   if (!req.user) {
+//     // The user is not logged in, send back an empty object
+//     res.json({});
+//   }
+//   else {
+//     // Otherwise send back the user's email and id
+//     // Sending back a password, even a hashed password, isn't a good idea
+//     res.json({
+//       email: req.user.email,
+//       id: req.user.id
+//     });
+//   }
+// });
 
 //all users test route
 app.get("/allusers", function(req,res){
